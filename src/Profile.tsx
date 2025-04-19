fc172076c3beb8a95ce2f629fecc133877206d13
Ahmad Mazlan <mazlancreative@gmail.com>
2025-04-19 21:47:05 +0700

    useState

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
+
 type ProfileProps = {
     nama: string
     profesi: string
 }
 
-function Profile({ nama, profesi }: ProfileProps) {
+function Profile({ nama: initialNama, profesi }: ProfileProps) {
+    const [nama, setNama] = useState(initialNama)
+
+    const gantiNama = () => {
+        setNama(nama === 'Mazlan' ? 'Lanma' : 'Mazlan')
+    }
+
     return (
         <div style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
             <h2>Nama: {nama}</h2>
             <p>Profesi: {profesi}</p>
+            <button onClick={gantiNama}>Ganti Nama</button>
         </div>
     )
 }
